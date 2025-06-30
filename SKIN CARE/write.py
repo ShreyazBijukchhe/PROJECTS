3959128aa724092c3dce90088a2e345320f83b69
ShreyazBijukchhe <bijukchheshreyaz@gmail.com>
2025-06-30 21:47:38 +0545

    Remove watch web and Skincare projects

--- SKIN CARE/write.py
+++
@@ -1,7 +0,0 @@
-def update_inventory_file(file_path, inventory):
-    """Update the inventory text file with current stock and prices"""
-    with open(file_path, 'w') as file:
-        file.write("Brand,Product Name,Country of Origin,Quantity,Cost Price in rupees\n")
-        for product in inventory:
-            file.write(f"{product['Brand']},{product['Product']},{product['Country']},"
-                      f"{product['Stock']},{product['Cost Price']}\n")
